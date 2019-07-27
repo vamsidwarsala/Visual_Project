0c460be3274b2a19146f660271ae51dd2d8d5fbd
dwarsala <vm420113@dal.ca>
2019-07-27 16:55:37 -0300

    prediction.js

--- Project/frontend/prediction.js
+++ Project/frontend/prediction.js
@@ -8,10 +8,7 @@ const plot_nyc_map = {
             .scale(55000)
             .translate([(width) / 2, (height) / 2]);
 
-        var path = d3.geo.path()
-            .projection(projection);
-
-
+       
 
         var svg = d3.select(".columncenter").append("svg")
             .attr("width", width)
@@ -31,13 +28,14 @@ const plot_nyc_map = {
                 burrowDropdown.append("option")
                     .attr("value", element)
                     .text(element);
+            plot()
             })
         });
 
 
-            function plot(data) {
+            function plot() {
 
-                d3.json("csvjson.json", function ( NYC_MapInfo) {
+                d3.json("../NYC_mapcoordinates.json", function ( NYC_MapInfo) {
 
                     // after loading geojson, use d3.geo.centroid to find out 
                     // where you need to center your map
@@ -89,23 +87,23 @@ const plot_nyc_map = {
                         .attr("d", path)
                         .attr("fill", "gray");
 
-                    svg.selectAll("circle")
-                        .data(data).enter()
-                        .append("circle")
-                        .on("mouseover", mouse_over)
-                        .on("mouseleave", mouse_leave)
-                        .attr("cx", function (d) {
-                            return projection([d.LONGITUDE, d.LATITUDE])[0]
-                        })
-                        .attr("cy", function (d) {
-                            return projection([d.LONGITUDE, d.LATITUDE])[1]
-                        })
-                        .attr("r", "1.5px")
-                        .attr("fill", "orange")
+                    // svg.selectAll("circle")
+                    //     .data(data).enter()
+                    //     .append("circle")
+                    //     .on("mouseover", mouse_over)
+                    //     .on("mouseleave", mouse_leave)
+                    //     .attr("cx", function (d) {
+                    //         return projection([d.LONGITUDE, d.LATITUDE])[0]
+                    //     })
+                    //     .attr("cy", function (d) {
+                    //         return projection([d.LONGITUDE, d.LATITUDE])[1]
+                    //     })
+                    //     .attr("r", "1.5px")
+                    //     .attr("fill", "orange")
                     
-                        svg.selectAll("circle")
-                        .data(data)
-                        .exit().remove()
+                    //     svg.selectAll("circle")
+                    //     .data(data)
+                    //     .exit().remove()
                 });
             }
 
