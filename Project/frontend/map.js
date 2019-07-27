5e45cba742c8aab90600c456b1d8938a09d8b53c
dwarsala <vm420113@dal.ca>
2019-07-27 12:16:08 -0300

    map js changes

--- Project/frontend/map.js
+++ Project/frontend/map.js
@@ -1,142 +1,109 @@
 const plot_nyc_map = {
-
     init: function (width, height) {
-
-        
         var projection = d3.geo.mercator()
             .center([-73.94, 40.70])
             .scale(55000)
             .translate([(width) / 2, (height) / 2]);
-
-      
         var svg = d3.select(".columncenter").append("svg")
             .attr("width", width)
             .attr("height", height);
-
         d3.csv("../nyc_data.csv", function (error, collisions) {
-
             d3.selectAll(".option").on("change", update);
             update();
             function update() {
                 var choices = [];
-                d3.selectAll(".option").each(function(d){
+                d3.selectAll(".option").each(function (d) {
                     cb = d3.select(this);
-                    if(cb.property("checked")){
-                      choices.push(cb.property("value"));
+                    if (cb.property("checked")) {
+                        choices.push(cb.property("value"));
                     }
-                  });
-                console.log(choices)          
-                var collisions_1 = []
+                });
+                var collisions_choices = []
                 if (choices.includes('CI')) {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF CYCLIST INJURED'] > 0) {
-                            collisions_1.push(collisions[i])
-                            
-                            var cyclist_injured_value = collisions_1.length
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
                             document.getElementById("cyclist_injured").innerHTML = cyclist_injured_value;
                         }
                     }
                 } else if (choices.includes('CD')) {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF CYCLIST KILLED'] > 0) {
-                            collisions_1.push(collisions[i])
-
-                            var cyclist_injured_value = collisions_1.length
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
                             document.getElementById("cyclist_killed").innerHTML = cyclist_injured_value;
                         }
                     }
-
-                }else if (choices.includes('MI')) {
+                } else if (choices.includes('MI')) {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF MOTORIST INJURED'] > 0) {
-                            collisions_1.push(collisions[i])
-
-                            var cyclist_injured_value = collisions_1.length
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
                             document.getElementById("motorist_injured").innerHTML = cyclist_injured_value;
                         }
                     }
-                    console.log("mi",collisions_1.length)          
-
-                }else if (choices.includes('MD')) {
+                } else if (choices.includes('MD')) {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF MOTORIST KILLED'] > 0) {
-                            collisions_1.push(collisions[i])
-
-                            var cyclist_injured_value = collisions_1.length
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
                             document.getElementById("motorist_killed").innerHTML = cyclist_injured_value;
                         }
                     }
-                    console.log("md",collisions_1.length)          
-
                 }
                 else if (choices.includes('PI')) {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF PEDESTRIANS INJURED'] > 0) {
-                            collisions_1.push(collisions[i])
-
-                            var cyclist_injured_value = collisions_1.length
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
                             document.getElementById("pedestrian_injured").innerHTML = cyclist_injured_value;
                         }
                     }
-
                 }
                 else if (choices.includes('PD')) {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF PEDESTRIANS KILLED'] > 0) {
-                            collisions_1.push(collisions[i])
-                            var cyclist_injured_value = collisions_1.length
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
                             document.getElementById("pedestrian_killed").innerHTML = cyclist_injured_value;
                         }
                     }
-
                 }
-                else{
+                else {
                     for (var i = 0; i < collisions.length; i++) {
                         if (collisions[i]['NUMBER OF PERSONS INJURED'] > 0 || collisions[i]['NUMBER OF PERSONS KILLED'] > 0) {
-                            collisions_1.push(collisions[i])
-                            var cyclist_injured_value = collisions_1.length
-                            document.getElementById("people_injured").innerHTML = cyclist_injured_value;                            
+                            collisions_choices.push(collisions[i])
+                            var cyclist_injured_value = collisions_choices.length
+                            document.getElementById("people_injured").innerHTML = cyclist_injured_value;
                         }
                     }
-
                 }
-                plot(collisions_1)
+                plot(collisions_choices)
             }
-
             function plot(data) {
-
                 d3.json("../NYC_mapcoordinates.json", function (error, NYC_MapInfo) {
-
                     // after loading geojson, use d3.geo.centroid to find out 
                     // where you need to center your map
                     var center = d3.geo.centroid(NYC_MapInfo);
                     projection.center(center);
-
                     // now you can create new path function with 
                     // correctly centered projection
                     var path = d3.geo.path().projection(projection);
-
-
                     var tooltip = d3.select("body")
                         .append("div")
                         //calling tooltip class in css
                         .attr("class", "tooltip_map")
                     var mouse_over = function (element) {
-                        console.log("ele", element)
                         tooltip.style("opacity", 1)
                             .html("<table bgcolor='FFFFFF'style='border: 1px solid black'><tr><td>" + "<b>Date</b>" + "<td>" + element.DATE + "</td></tr>" +
                                 "<tr><td>" + "<b>Time</b>" + "<td>" + element.TIME + "</td></tr>" +
-                                "<tr><td>" + "<b>Location</b>" + "<td>" +'('+ element.LATITUDE+','+element.LONGITUDE + ')'+ "</td></tr>" + 
-                                "<tr><td>" + "<b>Streetname</b>" + "<td>" +element['ON STREET NAME']+ "</td></tr>" +
-                                "<tr><td>" + "<b>Collision reason</b>" + "<td>" +element['CONTRIBUTING FACTOR VEHICLE 1']+ "</td></tr>" +
-                                "<tr><td>" + "<b>Vehicle Involved</b>" + "<td>" +element['VEHICLE TYPE CODE 1']+ "</td></tr>")                          
-
-                            // .html("<b>Fault</b> " + element.data.id + "<br>" + 
-                            // "Fault type " + element.data.data.parentId + "<br>" +
-                            // "Count " + element.data.data.count + "<br>")
+                                "<tr><td>" + "<b>Location</b>" + "<td>" + '(' + element.LATITUDE + ',' + element.LONGITUDE + ')' + "</td></tr>" +
+                                "<tr><td>" + "<b>Streetname</b>" + "<td>" + element['ON STREET NAME'] + "</td></tr>" +
+                                "<tr><td>" + "<b>Collision reason</b>" + "<td>" + element['CONTRIBUTING FACTOR VEHICLE 1'] + "</td></tr>" +
+                                "<tr><td>" + "<b>Vehicle Involved</b>" + "<td>" + element['VEHICLE TYPE CODE 1'] + "</td></tr>")
                             .style("right", "10px")
                             .style("top", "300px")
-                        //.style("background-color", color.transform(element['quality']))
                         d3.select(this)
                             .style("stroke", "black")
                             .style("opacity", 1)
@@ -148,9 +115,6 @@ const plot_nyc_map = {
                             .style("stroke", "none")
                             .style("opacity", 0.8)
                     }
-
-
-                    // and finally draw the actual polygons
                     svg.selectAll("path")
                         .data(NYC_MapInfo.features)
                         .enter()
@@ -171,20 +135,15 @@ const plot_nyc_map = {
                         })
                         .attr("r", "1.5px")
                         .attr("fill", "orange")
-                    
-                        svg.selectAll("circle")
+
+                    svg.selectAll("circle")
                         .data(data)
                         .exit().remove()
                 });
             }
 
         });
-
-
-
     },
-
-
 }
 document.addEventListener("DOMContentLoaded", function (radVizEvent) {
     plot_nyc_map.init(800, 700);
