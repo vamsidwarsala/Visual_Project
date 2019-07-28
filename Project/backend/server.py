1fee1a99aab0f513ee8c30e1bfc974a2aaaf361d
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2019-07-28 19:37:11 -0300

    Added code for model creation

--- Project/backend/server.py
+++ Project/backend/server.py
@@ -15,11 +15,90 @@ from sklearn.externals import joblib
 import pickle
 from datetime import datetime
 import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.externals import joblib
+
 
 
 app = Flask(__name__)
 cors = CORS(app)
 
+@app.route('/buildmodel', methods=['POST'])
+@cross_origin()
+def build_model():
+    collision_df = pd.read_csv("D:\\MACS\\Visual Analytics\\Project\\NYPD_Motor_Vehicle_Collisions.csv")
+    #dropping the unnecessary columns which have large number of number of null values
+    collision_df.drop(['ZIP CODE','LOCATION', 'ON STREET NAME', 'CROSS STREET NAME','OFF STREET NAME','CONTRIBUTING FACTOR VEHICLE 1', 'CONTRIBUTING FACTOR VEHICLE 2','CONTRIBUTING FACTOR VEHICLE 3', 'CONTRIBUTING FACTOR VEHICLE 4','CONTRIBUTING FACTOR VEHICLE 5', 'UNIQUE KEY', 'VEHICLE TYPE CODE 1','VEHICLE TYPE CODE 2', 'VEHICLE TYPE CODE 3', 'VEHICLE TYPE CODE 4','VEHICLE TYPE CODE 5'], axis=1, inplace=True)
+    #Replace unspecified values with NaN
+    collision_df = collision_df.replace('Unspecified', np.nan)
+    #Dropping the null values
+    collision_df.dropna(inplace=True)
+
+    #Sum all the injured or killed columns in to Total column and use it as the target label
+    collision_df['Total']= collision_df.iloc[:, 5:14].sum(axis=1)
+    collision_df_slice = collision_df.iloc[0:200000,:]
+    
+    collision_df_positive = collision_df.iloc[200001:600000,:]
+    #Extracting positive class data points
+    collision_df_positive_points = collision_df_positive[collision_df_positive['Total'] != 0]
+    # Adding positive class dataframe to handle imbalanced classes
+    collision_df = pd.concat([collision_df_slice,collision_df_positive_points], ignore_index=True)
+
+    #Converting all positive class data points to 1 code referenced from https://stackoverflow.com/questions/47382933/replace-non-zero-values-with-1
+    collision_df['Total'] = collision_df['Total'].astype(bool).astype(int)
+
+    # Shuffle the dataframe code referenced from https://stackoverflow.com/questions/15772009/shuffling-permutating-a-dataframe-in-pandas?noredirect=1&lq=1
+    collision_df = collision_df.reindex(np.random.permutation(collision_df.index))
+    collision_df.reset_index(drop=True, inplace=True)
+
+    collision_df.drop(['NUMBER OF PERSONS INJURED', 'NUMBER OF PERSONS KILLED',
+        'NUMBER OF PEDESTRIANS INJURED', 'NUMBER OF PEDESTRIANS KILLED',
+        'NUMBER OF CYCLIST INJURED', 'NUMBER OF CYCLIST KILLED',
+        'NUMBER OF MOTORIST INJURED', 'NUMBER OF MOTORIST KILLED'], axis=1, inplace=True)
+
+    #Converting the DATE and TIME columns to datetime format columns
+    collision_df['DATE'] = pd.to_datetime(collision_df.DATE)
+    collision_df['TIME'] = pd.to_datetime(collision_df.TIME)
+
+    #Converting the hour, month and day features to cyclic features code referenced from http://blog.davidkaleko.com/feature-engineering-cyclical-features.html
+    collision_df['HOUR_SINE'] = np.sin((collision_df['TIME'].dt.hour)*(2.*np.pi/24))
+    collision_df['HOUR_COSINE'] = np.cos((collision_df['TIME'].dt.hour)*(2.*np.pi/24))
+    collision_df['MONTH_SINE'] = np.sin((collision_df['DATE'].dt.month)*(2.*np.pi/12))
+    collision_df['MONTH_COSINE'] = np.cos((collision_df['DATE'].dt.month)*(2.*np.pi/12))
+    collision_df['DAY_SINE'] = np.sin((collision_df['DATE'].dt.day)*(2.*np.pi/30))
+    collision_df['DAY_COSINE'] = np.cos((collision_df['DATE'].dt.day)*(2.*np.pi/30))
+
+    #Converting the Latitude and Logitude to useful features for the algorithm code referenced from  https://datascience.stackexchange.com/questions/13567/ways-to-deal-with-longitude-latitude-feature
+    collision_df['LATITUDE_AND_LONGITUDE_X'] = np.cos(collision_df['LATITUDE']) * np.cos(collision_df['LONGITUDE'])
+    collision_df['LATITUDE_AND_LONGITUDE_Y'] = np.cos(collision_df['LATITUDE']) * np.sin(collision_df['LONGITUDE']) 
+    collision_df['LATITUDE_AND_LONGITUDE_Z'] = np.sin(collision_df['LATITUDE'])
+
+    #Dropping the columns DATA, TIME, LATITUDE and LONGITUDE after making the necessary conversions
+    collision_df = collision_df.drop(["DATE", 'TIME', 'LATITUDE', 'LONGITUDE'], axis=1)
+
+    #Converting the categorical features to one hot encoding using pd.get_dummies
+    collision_df = pd.get_dummies(collision_df)
+
+    #Taking the features from the dataframe
+    collision_df_X_features = collision_df.drop('Total', axis=1)
+    #Taking the target column which is the Total column
+    collision_df_y_target = collision_df.Total
+
+    #Splitting the dataset to train and test set in the ratio 70:30
+    collision_df_features_train, collision_df_features_test, collision_df_target_train, collision_df_target_test = train_test_split(collision_df_X_features, collision_df_y_target, test_size=0.3, random_state=34)
+
+    #After running random forest algorithm using grid_search cv we have taken the best parameters that the algorithm has given
+    #and used them for the final model.
+    random_forest_classifier = RandomForestClassifier(n_estimators=300, max_depth=10,max_features= 'sqrt',random_state=0, n_jobs=-1)
+    random_forest_classifier.fit(collision_df_features_train,collision_df_target_train)
+    #Saving the final random_forest_classifier model for predictions
+    file_name_path = 'C:\\Users\\Abraham Vineel\\Desktop\\final_random_forest_model.sav'
+    joblib.dump(random_forest_classifier, file_name_path)
+    return "Model Successfully built and Saved"
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
--- Project/frontend/map.js
+++ Project/frontend/map.js
@@ -1,15 +1,26 @@
 const plot_nyc_map = {
     init: function (width, height) {
-        var projection = d3.geo.mercator()
+
+        //The code is referenced from https://stackoverflow.com/questions/35972269/creating-a-d3-map-of-nyc-boroughs-using-js-and-a-geojson-file
+        //and http://bl.ocks.org/jhubley/64623ba08c2ee33ca43c
+
+        //Creating the map mercator with center [-73.94, 40.70] which is the center for the new york map
+        //The scale helps to position the map
+        var nyc_map_projection = d3.geo.mercator()
             .center([-73.94, 40.70])
             .scale(55000)
             .translate([(width) / 2, (height) / 2]);
+
         var svg = d3.select(".columncenter").append("svg")
             .attr("width", width)
             .attr("height", height);
+
+        //The nyc_data.csv 
         d3.csv("../nyc_data.csv", function (error, collisions) {
             d3.selectAll(".option").on("change", update);
             update();
+            //Function update() to add number of injuries or fatalities that have occurred to cyclists, mototrists and pedestrians 
+            //when the filter changes
             function update() {
                 var choices = [];
                 d3.selectAll(".option").each(function (d) {
@@ -79,20 +90,22 @@ const plot_nyc_map = {
                         }
                     }
                 }
-                plot(collisions_choices)
+                plot_map(collisions_choices)
             }
-            function plot(data) {
-                d3.json("../NYC_mapcoordinates.json", function (error, NYC_MapInfo) {
-                    // after loading geojson, use d3.geo.centroid to find out 
-                    // where you need to center your map
-                    var center = d3.geo.centroid(NYC_MapInfo);
-                    projection.center(center);
-                    // now you can create new path function with 
-                    // correctly centered projection
-                    var path = d3.geo.path().projection(projection);
+
+            //NYC_mapcoordinates.json is taken from http://data.beta.nyc//dataset/3bf5fb73-edb5-4b05-bb29-7c95f4a727fc/resource/6df127b1-6d04-4bb7-b983-07402a2c3f90/download/f4129d9aa6dd4281bc98d0f701629b76nyczipcodetabulationareas.geojson.
+            //These coorddinates are used to plot the new york map dataForNewYorkMap
+            //The code is referenced from https://stackoverflow.com/questions/35972269/creating-a-d3-map-of-nyc-boroughs-using-js-and-a-geojson-file
+            //and http://bl.ocks.org/jhubley/64623ba08c2ee33ca43c
+            function plot_map(data) {
+                d3.json("../NYC_mapcoordinates.json", function (error, dataForNewYorkMap) {
+                    
+                    var nycMapCenter = d3.geo.centroid(dataForNewYorkMap);
+                    nyc_map_projection.center(nycMapCenter);
+                    
+                    var path = d3.geo.path().projection(nyc_map_projection);
                     var tooltip = d3.select("body")
                         .append("div")
-                        //calling tooltip class in css
                         .attr("class", "tooltip_map")
                     var mouse_over = function (element) {
                         tooltip.style("opacity", 1)
@@ -116,7 +129,7 @@ const plot_nyc_map = {
                             .style("opacity", 0.8)
                     }
                     svg.selectAll("path")
-                        .data(NYC_MapInfo.features)
+                        .data(dataForNewYorkMap.features)
                         .enter()
                         .append("path")
                         .attr("d", path)
@@ -128,10 +141,10 @@ const plot_nyc_map = {
                         .on("mouseover", mouse_over)
                         .on("mouseleave", mouse_leave)
                         .attr("cx", function (d) {
-                            return projection([d.LONGITUDE, d.LATITUDE])[0]
+                            return nyc_map_projection([d.LONGITUDE, d.LATITUDE])[0]
                         })
                         .attr("cy", function (d) {
-                            return projection([d.LONGITUDE, d.LATITUDE])[1]
+                            return nyc_map_projection([d.LONGITUDE, d.LATITUDE])[1]
                         })
                         .attr("r", "1.5px")
                         .attr("fill", "orange")
@@ -146,6 +159,6 @@ const plot_nyc_map = {
     },
 }
 document.addEventListener("DOMContentLoaded", function () {
-    plot_nyc_map.init(800, 700);
+    plot_nyc_map.init(710, 700);
 });
 
